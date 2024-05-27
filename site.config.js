d24d00dcf3bcd074b079ed01fbe9e3e0c73d4c85
hkdkfih <info@lemon.line.pm>
2024-05-27 13:05:42 +0200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -23,7 +23,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "hkdkfih-blog-git-main-hhchenhhhhhhhs-projects.vercel.app",
+  link: "hkdkfih-blog.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
